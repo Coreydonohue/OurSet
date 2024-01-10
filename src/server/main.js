571e2119b916e2e992c75a2ccd5115e5a48bfea7
Coreydonohue <cqdonohue@gmail.com>
2024-01-10 15:25:39 -0500

    initial confic

---
+++ src/server/main.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// const socket = require("socket.io");
+
+app.use(cors())
+
+app.use(express.json({limit: "200mb"}));
+app.use(express.urlencoded({limit: "200mb", extended: true, parameterLimit:50000}));
+app.use(express.text({limit:'200mb'}));
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/api", require("./api"));
+
+app.listen(PORT, () => {
+    console.log(`Howdy - Server is listening on port ${PORT}...`);
+  });
+
+  
